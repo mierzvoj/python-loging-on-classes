3808abe8efa2a257d263476977d037d494c875a6
mierzvoj <mierzvoj@gmail.com>
2022-03-23 21:04:21 +0100

    2nd commit

--- LoggingApiModule.py
+++ LoggingApiModule.py
@@ -1,20 +1,23 @@
+from UserModule import User
 class LoggingApi:
+
+    user = User
 
     def __init__(self):
         self = self
 
-    def login(self):
+    def login(self, user):
         self.username = input("Podaj swój login: ")
         self.password = input("Podaj swoje hasło: ")
         with open('users.csv', 'r') as csvfile:
             if self.username == self.username and self.password == self.password:
-                print("Zalogowałeś się")
+                print("Zalogowałeś się do newapi")
             else:
                 print("Logowanie nie powiodło się ")
 
-    def register(self):
+    def register(self, user):
         self.username = input("Podaj swój login, celem rejestracji: ")
-        self.password = input("Podaj hasło, aby dokończyć rrjestrację: ")
+        self.password = input("Podaj hasło, aby dokończyć rejestrację: ")
         self.passwordcheck = input("Podaj ponownie hasło, celem weryfikacji: ")
         with open("users.csv", 'a', newline='') as csvfile:
             csvfile.write("Użytkownik:", self.username + ",", "Hasło:", self.password + ",")
@@ -22,3 +25,18 @@ class LoggingApi:
             print("Hasła pasują do siebie, zaloguj się ")
         else:
             print("Hasła się różnią")
+
+    def options(self):
+            print("Witamy w programie logowania\n")
+            success = True
+            while success:
+                print("Wybierz opcję menu lista: 1/szukaj: 2/usun: 3/zakoncz: 4\n")
+                menu = int(input("podaj wybór: "))
+                if menu == 1:
+                    print("tu 1")
+                elif menu == 2:
+                    print("tu 2")
+                elif menu == 3:
+                    print("tu 3")
+                else:
+                    exit()
--- main.py
+++ main.py
@@ -8,18 +8,18 @@ def run():
 
 def begin():
     newapi = createNewLoggingApi()
-    newuser = createNewUser()
+
     print("Witaj")
     while True:
-
         option = int(input("Zaloguj albo zarejestruj się: 1 lub 2 "))
         if option in ['1', '2']:
             break
         if option == 1:
             newapi.login()
         else:
-            createNewUser()
-            newapi.register(newuser)
+            newuser = createNewUser()
+            newapi.login(newuser)
+        newapi.options()
 
 
 def createNewLoggingApi():
@@ -28,8 +28,8 @@ def createNewLoggingApi():
 
 
 def createNewUser():
-    username = input("Podaj swój login / username: ")
-    password = input("Podaj swoje hasło: ")
+    username = input("Utwórz konto użytkownika, podaj swój login / username: ")
+    password = input("Podaj swoje nowe hasło: ")
     newuser = User(username=username, password=password)
     return newuser
 
